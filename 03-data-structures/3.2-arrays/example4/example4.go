f048a19c8fbc1d5a4a6d4d4da37063a4e5e51925
j.lazerson <jake.lazerson@valuecentric.com>
2020-06-17 02:33:32 -0400

    BH - go

--- 03-data-structures/3.2-arrays/example4/example4.go
+++ 03-data-structures/3.2-arrays/example4/example4.go
@@ -1,5 +1,4 @@
-// All material is licensed under the Apache License Version 2.0, January 2004
-// http://www.apache.org/licenses/LICENSE-2.0
+// B''H
 
 // Sample program to show how the for range has both value and pointer semantics.
 package main
@@ -8,21 +7,23 @@ import "fmt"
 
 func main() {
 
+	// -- -------------------------------------
 	// Using the pointer semantic form of the for range.
 	friends := [5]string{"Annie", "Betty", "Charley", "Doug", "Edward"}
-	fmt.Printf("Bfr[%s] : ", friends[1])
+	fmt.Printf("Before[%s] : ", friends[1])
 
 	for i := range friends {
 		friends[1] = "Jack"
 
 		if i == 1 {
-			fmt.Printf("Aft[%s]\n", friends[1])
+			fmt.Printf("After[%s]\n", friends[1])
 		}
 	}
 
+	// -- -------------------------------------
 	// Using the value semantic form of the for range.
 	friends = [5]string{"Annie", "Betty", "Charley", "Doug", "Edward"}
-	fmt.Printf("Bfr[%s] : ", friends[1])
+	fmt.Printf("Before[%s] : ", friends[1])
 
 	for i, v := range friends {
 		friends[1] = "Jack"
@@ -32,10 +33,11 @@ func main() {
 		}
 	}
 
+	// -- -------------------------------------
 	// Using the value semantic form of the for range but with pointer
 	// semantic access. DON'T DO THIS.
 	friends = [5]string{"Annie", "Betty", "Charley", "Doug", "Edward"}
-	fmt.Printf("Bfr[%s] : ", friends[1])
+	fmt.Printf("Before[%s] : ", friends[1])
 
 	for i, v := range &friends {
 		friends[1] = "Jack"
