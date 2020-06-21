cb21b597bbfcadb33a1d4c0a1363e086a7f5bdf7
j.lazerson <jake.lazerson@valuecentric.com>
2020-06-21 08:16:50 -0400

    BH githubz

--- 00-playground/githubz/main.go
+++ 00-playground/githubz/main.go
@@ -20,7 +20,7 @@ import (
 )
 
 //!+template
-const templ = `{{.TotalCount}} issues:
+const templ = `{{.TotalCount}} issues sdklfjsdklfj:
 {{range .Items}}----------------------------------------
 Number: {{.Number}}
 User:   {{.User.Login}}
@@ -43,51 +43,14 @@ var report = template.Must(template.New("issuelist").
 	Parse(templ))
 
 func main() {
+
 	result, err := github.SearchIssues(os.Args[1:])
+
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	if err := report.Execute(os.Stdout, result); err != nil {
 		log.Fatal(err)
 	}
 }
-
-//!-exec
-
-func noMust() {
-	//!+parse
-	report, err := template.New("report").
-		Funcs(template.FuncMap{"daysAgo": daysAgo}).
-		Parse(templ)
-	if err != nil {
-		log.Fatal(err)
-	}
-	//!-parse
-	result, err := github.SearchIssues(os.Args[1:])
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err := report.Execute(os.Stdout, result); err != nil {
-		log.Fatal(err)
-	}
-}
-
-/*
-//!+output
-$ go build gopl.io/ch4/issuesreport
-$ ./issuesreport repo:golang/go is:open json decoder
-13 issues:
-----------------------------------------
-Number: 5680
-User:   eaigner
-Title:  encoding/json: set key converter on en/decoder
-Age:    750 days
-----------------------------------------
-Number: 6050
-User:   gopherbot
-Title:  encoding/json: provide tokenizer
-Age:    695 days
-----------------------------------------
-...
-//!-output
-*/
